202f8c9121d2befd4d082d51de07dbd95026c3f4
David Hui <BlazingAsher@users.noreply.github.com>
2020-05-10 23:52:59 -0400

    include hash of password as hint

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import traceback
 import multiprocessing
+import hashlib
 
 
 def comm(conn, addr):
@@ -23,7 +24,7 @@ def comm(conn, addr):
         if ''.join(buffer[:16]) == ''.join(buffer[16:32]):
             conn.sendall("AJD$!(#HD!B".encode('utf-8'))
         else:
-            conn.sendall("Incorrect!".encode('utf-8'))
+            conn.sendall("Incorrect! Reminder: The SHA256 hash of the password is {}".format(hashlib.sha256(''.join(buffer[16:32]).encode('utf-8')).hexdigest()).encode('utf-8'))
 
     except:
         conn.sendall(b'Error. Please try again')
